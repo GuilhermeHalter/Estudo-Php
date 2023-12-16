4873533f5938e7106b086021ccaecc300cfbd028
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2023-12-15 22:27:13 -0300

    Organizando pasta e feito exercicio2.

---
+++ Exercicios/exercicio2.php
@@ -0,0 +1,12 @@
+<?php
+
+// Escreva um script que pede o raio de um círculo, e em seguida exiba o perímetro e área do círculo.
+
+ $pi = 3.14;
+ $raio = readline("Digite o raio do circulo:");
+ $perimetro = (2.$pi) * $raio;
+ $area = $pi * ($raio**2);
+ 
+ echo "O perimetro do círculo é ". $perimetro . " e a área do círculo é ". $area;
+
+?>
