3da7a9a5c3b0878c6efd96131fc3c3bc85be9a6e
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2023-12-15 22:39:01 -0300

    feito exercicio 4

---
+++ Exercicios/exercicio4.php
@@ -0,0 +1,11 @@
+<?php
+ //Faça um script que peça dois números e imprima a soma.
+ 
+ $numero1 = readline("Informe um número:");
+ $numero2 = readline("Informe outro número: ");
+ 
+ $soma = $numero1 + $numero2;
+
+ echo $soma;
+
+?>
