3d896609ab7bf428cd4b997e60bfe4a78b35b978
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2023-12-19 14:03:18 -0300

    feito exercicio 6

--- Exercicios/exercicio6.php
+++ Exercicios/exercicio6.php
@@ -1,5 +1,9 @@
 <?php
 //Faça um script que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
 
+ $lado = readline("Informe o tamanho dos lados do quadrado:");
+ $area = $lado * $lado;
+ $dobro = $area * 2 ;
+ echo "o dobro da área do quadrado é ". $dobro
 
 ?>
